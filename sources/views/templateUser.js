d318f960f7a53c763b671626cfcc3b931fe66721
Noctaum <Noctaum@users.noreply.github.com>
2018-04-11 10:40:05 +0300

    add user photo

--- sources/views/templateUser.js
+++ sources/views/templateUser.js
@@ -48,9 +48,8 @@ export default class templateUser extends JetView{
 					},
 					{cols:[
 						{
-							template:"Картинка", 
-							// height:100,
-							// width:100
+							template:"<div class='userImageWrape'><img class='userPhoto' src='http://milkyway.mie.uc.edu/cgdm/students/Male.png'></div>", 
+					
 						},
 						{rows:[
 							{	
