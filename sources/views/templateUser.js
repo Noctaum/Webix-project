02fd270fd29ba8e3fd80e68ed6b6d7f4964e0acc
Noctaum <Noctaum@users.noreply.github.com>
2018-04-11 23:40:15 +0300

    add one big template, it has problem with double qoute when use lint

--- sources/views/templateUser.js
+++ sources/views/templateUser.js
@@ -1,6 +1,5 @@
 import {JetView} from "webix-jet";
 import {contacts} from "models/contacts";
-//import {status} from "models/status";
 
 export default class templateUser extends JetView{
 
@@ -27,68 +26,19 @@ export default class templateUser extends JetView{
 			}
 		};
 
+		let templ = (data) =>{
+			return `<div><h2>${data.FirstName ? data.FirstName : 'empty'} ${data.LastName ? data.LastName : 'empty'}</h2></div><div class="bigCantainer"><div class="conteiner"><div class='userImageWrape'><img class='userPhoto' src='http://milkyway.mie.uc.edu/cgdm/students/Male.png'></div><div class="content">${data.StatusID ? data.StatusID : 'empty'} </div></div><div class="conteiner"><div class="content"><span class='webix_icon fa-envelope'></span> ${data.Email ? data.Email : 'empty'}</div><div class="content"><span class='webix_icon fa-skype'></span> ${data.Skype ? data.Skype : 'empty'} </div><div class="content"><span class='webix_icon fa-tag'></span> ${data.Job ? data.Job : 'empty'}</div><div class="content"><span class='webix_icon fa-briefcase'></span> ${data.Company ? data.Company : 'empty'} </div></div><div class="conteiner"><div class="content"><span class='webix_icon fa-calendar'></span> ${data.Birthday ? data.Birthday : 'empty'} </div><div class="content"><span class='webix_icon fa-map-marker'></span> ${data.Address ? data.Address : 'empty'} </div></div></div>`;
+		};
+
+
 		let form =
-			{
-				gravity:4,
-				id:"formUser",
-				rows:[
-					{	
-						view:"template",
-						id:"head",
-						template:"<h2>#FirstName# #LastName#</h2>",
-						height:70
-					},
-					{cols:[
-						{
-							template:"<div class='userImageWrape'><img class='userPhoto' src='http://milkyway.mie.uc.edu/cgdm/students/Male.png'></div>", 
-					
-						},
-						{rows:[
-							{	
-								view:"template",
-								id:"mail",
-								template:"<span class='webix_icon fa-envelope'></span> #Email#",
-							},
-							{	
-								view:"template",
-								id:"skype",
-								template:"<span class='webix_icon fa-skype'></span> #Skype#",
-							},
-							{	
-								view:"template",
-								id:"job",
-								template:"<span class='webix_icon fa-tag'></span> #Job#",
-							},
-							{	
-								view:"template",
-								id:"comp",
-								template:"<span class='webix_icon fa-briefcase'></span> #Company#",
-							},
-						]},
-						{rows:[
-							{	
-								view:"template",
-								id:"birth",
-								template:"<span class='webix_icon fa-calendar'></span> #Birthday#",
-							},
-							{	
-								view:"template",
-								id:"map",
-								template:"<span class='webix_icon fa-map-marker'></span> #Address#",
-							},
-							{},
-							{}
-						]},
-
-					]},
-					{	
-						view:"template",
-						id:"status",
-						template:"#StatusID#"
-					},
-				]
-			}; 
-
+			{	
+				view:"template",
+				id:"head",
+				template:templ,
+				gravity: 6
+			};
+			
 		return {cols:[form, {rows:[saveBut,delBut,{}]}],gravity:4};
 	}
 	urlChange(){
@@ -97,13 +47,6 @@ export default class templateUser extends JetView{
 			if (id){
 				let data = contacts.getItem(id);
 				this.$$("head").setValues(data);
-				this.$$("mail").setValues(data);
-				this.$$("skype").setValues(data);
-				this.$$("job").setValues(data);
-				this.$$("comp").setValues(data);
-				this.$$("birth").setValues(data);
-				this.$$("map").setValues(data);
-				this.$$("status").setValues(data);
 			}
 		});
 	}
